aa93c43b31249aae2a5fa86467e898a322b53449
lunapr-dev <lunadevelopment1@gmail.com>
2024-09-27 11:24:25 +0400

    2ndcommit2

--- src/main.js
+++ src/main.js
@@ -18,3 +18,5 @@ const wagmiConfig = createClient({
 
 const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const web3modal = new Web3Modal({ projectId }, ethereumClient)
+
+console.log(web3modal)
