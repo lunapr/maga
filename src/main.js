d4b5b4dd3a4377886c70bc583cfb9f78d77f1769
lunapr-dev <lunadevelopment1@gmail.com>
2024-09-27 11:21:32 +0400

    2ndcommit

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import { configureChains, createClient } from '@wagmi/core'
 import { arbitrum, mainnet, polygon } from '@wagmi/core/chains'
-import { EthereumClient, w3mConnectors, W3mProvider } from '@web3modal/ethereum' 
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 
 const chains = [arbitrum, mainnet, polygon]
-const projectid = 'YOu.
 
-IR_PROJECT_ID'
+// Replace project ID
+const projectId = '0bb6cec1d957f7f04be82de3b98bce7d'
+
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
-const wagmiConfig = createConfig({
-autoConnect: true,
-connectors: W3mConnectors({ projectId, version: 1, chains }),
-publicClient
-const ethereumClient = new EthereumClient (wagmiConfig, chains)
+
+const wagmiConfig = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient,
+})
+
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 const web3modal = new Web3Modal({ projectId }, ethereumClient)
